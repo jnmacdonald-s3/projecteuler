e844804065af0f5daf4bc7c899eb56b5d25a0b09
jeffdiana <jeffrey.diana@s3group.com>
2015-06-04 14:30:10 -0400

    Added functional logging infrastructure

--- projecteuler.py
+++ projecteuler.py
@@ -9,6 +9,11 @@ Project Euler - An interactive project with the purposes of learning:
     - Introduction to Git and Github
     - Collaborative development with Git
 """
+# ******************************************************************************
+# Imports
+# ******************************************************************************
+
+import logging
 
 # ******************************************************************************
 # Globals
@@ -23,16 +28,56 @@ def problem1():
     """ Problem 1
     """
     
+    logger.info('test inside function')
+    logger.debug('does it work?')
+    
     print("Problem 1")
     return
 
 # ******************************************************************************
 # Main
 # ******************************************************************************
-def main():
 
+def main():
+    
+    # Logging Configuration ----------------------------------------------------
+    # create logger
+    global logger
+    logger = logging.getLogger('projecteuler')
+    logger.setLevel(logging.DEBUG)
+    
+    # create file handler & console handler setting them to appropiate levels
+    file_h = logging.FileHandler('projecteuler.log')
+    file_h.setLevel(logging.DEBUG)
+    
+    console_h = logging.StreamHandler()
+    console_h.setLevel(logging.INFO)
+    
+    # create formatters for each handler
+    con_format = logging.Formatter(
+        '[ %(funcName)s:%(lineno)d ] %(levelname)s  %(message)s'
+        )
+    log_format = logging.Formatter(
+        '%(asctime)s [ %(funcName)s:%(lineno)d ] %(levelname)s  %(message)s'
+        )
+    
+    # set formatters to appropriate
+    file_h.setFormatter(log_format)
+    console_h.setFormatter(con_format)
+    
+    # add handlers to logger
+    logger.addHandler(file_h)
+    logger.addHandler(console_h)
+    
+    # Problem Functions --------------------------------------------------------
+    logger.info('Program Started')
+    
+    # Function for each problem calls
     problem1()
 
+    logger.info('Program Completed')
+    logger.info('--------------------------------------\n')
+    
     return
 
 # ------------------------------------------------------------------------------
